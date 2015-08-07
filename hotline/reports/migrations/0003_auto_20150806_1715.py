e57f27540a6a3e21ff4fc9c43980bf68afe0a814
Matt Johnson <mdj2@pdx.edu>
2015-08-06 17:15:35 -0700

    Add in some missing migrations

---
+++ hotline/reports/migrations/0003_auto_20150806_1715.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0002_report_county'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='report',
+            name='edrr_status',
+            field=models.IntegerField(default=0, verbose_name='EDDR Status', choices=[(0, ''), (1, 'No Response/Action Required'), (2, 'Local expert notified'), (3, 'Population assessed'), (4, 'Population treated'), (5, 'Ongoing monitoring'), (6, 'Controlled at site')]),
+        ),
+        migrations.AlterField(
+            model_name='report',
+            name='has_specimen',
+            field=models.BooleanField(default=False, verbose_name='Do you have a physical specimen?'),
+        ),
+    ]
