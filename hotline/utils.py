125d5857cc2271c15daeb1ea761dc7624ba42bf1
Matt Johnson <mdj2@pdx.edu>
2015-08-19 15:29:53 -0700

    Minor polishing and a bug fix

--- hotline/utils.py
+++ hotline/utils.py
@@ -2,21 +2,20 @@ import subprocess
 
 
 def resize_image(input_path, output_path, width, height):
-    thumbnail_size = "%dx%d" % (width, height)
+    size = "%dx%d" % (width, height)
     return subprocess.call([
         "convert",
         input_path,
         "-thumbnail",
-        # the > below means don't enlarge images that fit in the 64x64 box
-        thumbnail_size + ">",
+        # the > below means don't enlarge images that fit in the box
+        size + ">",
         "-background",
         "transparent",
         "-gravity",
         "center",
-        # fill the 64x64 box with the background color (which
-        # is transparent) so all the thumbnails are exactly the
-        # same size
+        # fill the box with the background color (which
+        # is transparent)
         "-extent",
-        thumbnail_size,
+        size,
         output_path
     ])
