80efbc90a080d65e36a03df8ab9fa9cd97d48a1f
Matt Johnson <mdj2@pdx.edu>
2015-08-20 10:26:15 -0700

    Add back in the users-detail page...for now.

--- hotline/users/views.py
+++ hotline/users/views.py
@@ -114,6 +114,23 @@ def home(request):
     })
 
 
+class Detail(DetailView):
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(is_active=True)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        super(DetailView, self).get_object()
+        context = super(Detail, self).get_context_data(**kwargs)
+        context['current_user'] = self.request.user
+
+        return context
+
+
 @permissions.can_list_users
 def list_(request):
     """
