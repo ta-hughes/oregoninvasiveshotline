65262279980edd5a3c988f40b758d00639c8c643
Matt Johnson <mdj2@pdx.edu>
2015-08-20 15:10:30 -0700

    Fix: The species selector didn't work in all browsers, since display:none on <option> isn't well supported

--- hotline/static/js/species_selector.js
+++ hotline/static/js/species_selector.js
@@ -1,24 +1,30 @@
+/*
+ * When a category is selected we need to remove any species from the species
+ * drop down list that aren't in that category. Unfortunately, we can't just
+ * use `display:none` on the <option> tags, since that doesn't work in all
+ * browsers. If the category isn't selected, then hide the species drop down
+ */
 $(document).ready(function(){
-
-    // when a category is selected, we need to change up the species field
+    // keep a copy of the original list, since each time the category changes,
+    // we need to restore the original, and remove the irrelevant items
+    var original_list = $('#id_reported_species').html()
     $('body').on('change', '#id_reported_category', function(e){
         var category_id = $(this).val();
         if(category_id){
             $('#reported-species').show()
+            $('#id_reported_species').html(original_list)
 
-            // hide all the species and then foreach species in that category, show it
-            var species_ids = category_id_to_species_id[category_id];
-            $('#id_reported_species option').hide()
-            for(var i = 0; i < species_ids.length; i++){
-                $('#id_reported_species option[value="' + species_ids[i] + '"]').show();
-            }
-
-            // the first option is the "Unknown" option, so it should always be visible
-            $('#id_reported_species option:first').show()
-            // if the species that was selected was hidden, reset to the "Unknown" option
-            if($('#id_reported_species option:selected').css("display") == "none"){
-                $('#id_reported_species').val("");
-            }
+            var species_ids = category_id_to_species_id[category_id] || [];
+            $('#id_reported_species option').each(function(i){
+                // the first option is assumed to be the "Unknown" option,
+                // which we always want to keep
+                if(i == 0) return;
+                // if the option isn't in our species_ids list, then we don't
+                // want to show it
+                if(species_ids.indexOf(parseInt($(this).val())) == -1){
+                    $(this).remove()
+                }
+            });
         } else {
             $('#reported-species').hide()
         }
