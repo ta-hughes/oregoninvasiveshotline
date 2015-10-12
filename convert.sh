d7844e57c803ff7bb012243851d550d2ae74e16a
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-10-12 16:34:50 -0700

    Add path to find command in convert.sh for portability

--- convert.sh
+++ convert.sh
@@ -7,4 +7,4 @@ mkdir -p media/images
 cd /vol/www/invasivespecieshotline/invasivespecieshotline/public/uploads/images/0000
 function goo() { echo $1 | tr "/" "-"; }
 export -p goo
-find -type f | while read file; do cp $file /vol/www/invasivespecieshotline/prod/media/images/$(goo "${file:2}"); done
+find . -type f | while read file; do cp $file /vol/www/invasivespecieshotline/prod/media/images/$(goo "${file:2}"); done
