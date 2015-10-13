2e77934bdaecd6aaef246e9873833cdf270d0a19
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-10-12 17:29:43 -0700

    Clean up the custom test runner
    - Get rid of settings that are already defined in local settings
    - Get rid of unused Celery settings
    - Use setup and teardown methods appropriately

--- hotline/testrunner.py
+++ hotline/testrunner.py
@@ -3,26 +3,21 @@ import shutil
 import tempfile
 
 from django.conf import settings
+
 from elasticmodels import SearchRunner
 
 
 class TestRunner(SearchRunner):
-    """
-    Set whatever test settings need to be in place when you run tests
-    """
-    def run_tests(self, *args, **kwargs):
-        settings.TEST = True
-        media_root = tempfile.mkdtemp()
-        # these directories are needed for generated icons and thumbnails
-        os.mkdir(os.path.join(media_root, "generated_icons"))
-        os.mkdir(os.path.join(media_root, "generated_thumbnails"))
-        settings.MEDIA_ROOT = media_root
-        settings.CELERY_ALWAYS_EAGER = True
-        settings.PASSWORD_HASHERS = (
-            'django.contrib.auth.hashers.MD5PasswordHasher',
-        )
 
-        to_return = super().run_tests(*args, **kwargs)
-        shutil.rmtree(media_root)
+    def setup_test_environment(self, **kwargs):
+        settings.MEDIA_ROOT = media_root = tempfile.mkdtemp()
 
-        return to_return
+        # These directories are needed for generated icons and thumbnails
+        os.mkdir(os.path.join(media_root, 'generated_icons'))
+        os.mkdir(os.path.join(media_root, 'generated_thumbnails'))
+
+        super().setup_test_environment(**kwargs)
+
+    def teardown_test_environment(self, **kwargs):
+        shutil.rmtree(settings.MEDIA_ROOT)
+        super().teardown_test_environment(**kwargs)
