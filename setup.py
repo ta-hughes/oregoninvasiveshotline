4f698bd0f3a3c592fea67f788cc73d0f087007fe
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-10-12 17:34:32 -0700

    Add setup.py so we can build proper source distributions

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '1.0.0.dev0'
+
+
+setup(
+    name='psu.oit.arc.oregoninvasiveshotline',
+    version=VERSION,
+    description='Oregon Invasives Hotline',
+    author='PSU - OIT - ARC',
+    author_email='consultants@pdx.edu',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django-arcutils>=1.1.1,<2.0',
+        'django-cloak',
+        'django-local-settings>=1.0a7',
+        'django-perms>=1.2.0',
+        'django>=1.8.5,<1.9',
+        'elasticmodels>=0.2.0',
+        'Markdown>=2.6.2',
+        'Pillow>=3.0.0',
+        'psycopg2>=2.6.1',
+        'pytz>=2015.6',
+    ],
+    extras_require={
+        'dev': [
+            'bpython',
+            'coverage',
+            'flake8',
+            'isort',
+            'model_mommy',
+            'mommy-spatial-generators',
+            'psu.oit.arc.tasks',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
