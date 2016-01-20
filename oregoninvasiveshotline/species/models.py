61b41c6eb853a605cd7bc331faedc63e72a4a33c
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-01-20 12:32:12 -0800

    Clean up the Species model
    - Remove class docstring that provides no useful info
    - Put Meta class at top
    - Clean up fields (alphabetize except PK field)
    - Add a title property that contains the logic that was previously in
      the __str__ method (cleaned up so it's understandable at a glance)
    - Standardize on single quotes
    
    The title property will make it easier to get species titles when, e.g.,
    indexing reports.

--- oregoninvasiveshotline/species/models.py
+++ oregoninvasiveshotline/species/models.py
@@ -51,25 +51,31 @@ class Severity(models.Model):
 
 
 class Species(models.Model):
-    """
-    A species that can be reported on
-    """
-    species_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    scientific_name = models.CharField(max_length=255, blank=True)
-    remedy = models.TextField(blank=True)
-    resources = models.TextField(blank=True)
-
-    is_confidential = models.BooleanField(default=False, help_text="""
-        A species can be marked as confidential if making a report about it public would cause harm
-    """)
-
-    severity = models.ForeignKey(Severity)
-    category = models.ForeignKey(Category)
 
     class Meta:
-        db_table = "species"
+        db_table = 'species'
         ordering = ['name']
 
+    species_id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category)
+    is_confidential = models.BooleanField(
+        default=False,
+        help_text=(
+            'A species can be marked as confidential if making a report about it public would '
+            'cause harm'
+        )
+    )
+    name = models.CharField(max_length=255)
+    remedy = models.TextField(blank=True)
+    resources = models.TextField(blank=True)
+    scientific_name = models.CharField(max_length=255, blank=True)
+    severity = models.ForeignKey(Severity)
+
+    @property
+    def title(self):
+        if self.scientific_name:
+            return '{0.name} ({0.scientific_name})'.format(self)
+        return self.name
+
     def __str__(self):
-        return "%s%s" % (self.name, " (%s)" % self.scientific_name if self.scientific_name else "")
+        return self.title
