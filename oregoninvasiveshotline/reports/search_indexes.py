e5a1aeb52f17157ee0bffd6a1e832ba535ae6ee8
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-01-20 13:55:13 -0800

    Clean up ReportIndex
    - Use generic `pk` where possible; for example, we can do
      `model_attr='category__pk` instead of
      `model_attr='category__category_id'`
    - Be explicit about where values are coming from where possible; for
      example, `model_attr='category__name'` instead of
      `model_attr='category'` (the latter implicitly does `str(category)`)
    - Keep some fields from being indexed; since we're not going to search
      on them, they don't need to be analyzed, just stored
    - Remove `index_queryset` method; instead, use the default
      implementation that indexes all reports
    - Get rid of `prepare_*` methods
      - Some aren't needed because the relevant models now have a `title`
        property
      - The `prepare_email` method was unused
    - Remove comments
    - Reorganize fields
    - Use single quotes consistently throughout

--- oregoninvasiveshotline/reports/search_indexes.py
+++ oregoninvasiveshotline/reports/search_indexes.py
@@ -1,77 +1,50 @@
-from django.utils import timezone
 from haystack import indexes
 
 from .models import Report
 
 
 class ReportIndex(indexes.SearchIndex, indexes.Indexable):
-    """
-    Indexes reports in Elasticsearch using Haystack
-    """
-    # Haystack requires one field to be used as the primary search term
+
     text = indexes.CharField(document=True, use_template=True)
 
-    report_id = indexes.IntegerField(model_attr="report_id")
+    report_id = indexes.IntegerField(model_attr='report_id')
 
-    species = indexes.CharField(model_attr="species__name", null=True)
-    species_id = indexes.CharField(model_attr="species__species_id", null=True)
-    reported_species = indexes.CharField(model_attr="reported_species__name", null=True)
-    actual_species = indexes.CharField(model_attr="actual_species__name", null=True)
+    title = indexes.CharField(model_attr='title')
+    description = indexes.CharField(model_attr='description')
+    location = indexes.CharField(model_attr='location')
 
-    category = indexes.CharField(model_attr="category")
-    category_id = indexes.IntegerField(model_attr="category__category_id")
+    edrr_status = indexes.CharField(model_attr='get_edrr_status_display')
+    ofpd = indexes.BooleanField(model_attr='created_by__has_completed_ofpd')
 
-    title = indexes.CharField()
-    description = indexes.CharField()
-    location = indexes.CharField()
-    county = indexes.CharField(model_attr="county__name", null=True)
-    county_id = indexes.IntegerField(model_attr="county__pk", null=True)
+    category = indexes.CharField(model_attr='category__name')
+    category_id = indexes.IntegerField(model_attr='category__pk')
 
-    edrr_status = indexes.CharField(model_attr="get_edrr_status_display")
-    ofpd = indexes.BooleanField(model_attr="created_by__has_completed_ofpd")
+    claimed_by = indexes.CharField(model_attr='claimed_by', null=True)
+    claimed_by_id = indexes.IntegerField(model_attr='claimed_by__user_id', null=True)
 
-    claimed_by = indexes.CharField(model_attr="claimed_by", null=True)
-    claimed_by_id = indexes.IntegerField(model_attr="claimed_by__user_id", null=True)
+    county = indexes.CharField(model_attr='county__name', null=True)
+    county_id = indexes.IntegerField(model_attr='county__pk', null=True)
 
-    created_by = indexes.CharField(model_attr="created_by")
-    created_by_id = indexes.IntegerField(model_attr="created_by__user_id")
-    created_on = indexes.DateField(model_attr="created_on")
+    created_by = indexes.CharField(model_attr='created_by')
+    created_by_id = indexes.IntegerField(model_attr='created_by__user_id')
+    created_on = indexes.DateTimeField(model_attr='created_on')
 
-    is_archived = indexes.BooleanField(model_attr="is_archived", default=False)
-    is_public = indexes.BooleanField(model_attr="is_public", default=False)
+    is_archived = indexes.BooleanField(model_attr='is_archived', default=False)
+    is_public = indexes.BooleanField(model_attr='is_public', default=False)
 
-    icon_url = indexes.CharField(model_attr="icon_url")
-    image_url = indexes.CharField(model_attr="image_url", default=None)
-    lat = indexes.FloatField(model_attr="point__y")
-    lng = indexes.FloatField(model_attr="point__x")
+    species_id = indexes.CharField(model_attr='species__pk', null=True)
+    species = indexes.CharField(model_attr='species__title', null=True)
+    reported_species = indexes.CharField(model_attr='reported_species__name', null=True)
+    actual_species = indexes.CharField(model_attr='actual_species__name', null=True)
+
+    # Non-indexed fields (i.e., fields that we don't search on but that
+    # we want available in search results).
+
+    icon_url = indexes.CharField(model_attr='icon_url', indexed=False)
+    image_url = indexes.CharField(model_attr='image_url', default=None, indexed=False)
+
+    lat = indexes.FloatField(model_attr='point__y', indexed=False)
+    lng = indexes.FloatField(model_attr='point__x', indexed=False)
 
     def get_model(self):
-        """
-        Returns the current model
-        """
         return Report
-
-    def index_queryset(self, using=None):
-        """
-        Used when the entire Report index is updated, indexes new document
-        """
-        return self.get_model().objects.filter(created_on__lte=timezone.now())
-
-    def prepare_email(self, model):
-        """
-        Prepare the email field
-        """
-        return None if model.claimed_by is None else model.claimed_by.email.lower()
-
-    def prepare_species(self, model):
-        """
-        Prepare species field, appends species scientific_name to species_name
-        """
-        return model.species.name + " " + model.species.scientific_name if model.species else None
-
-    def prepare_title(self, model):
-        """
-        Prepares the title field, describing the document in terms of the report
-        it represents.
-        """
-        return model.__str__()
