7d35b4b0306e6382aa54e1fee84539e8c5394dee
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-01-20 18:26:23 -0800

    Update wsgi.py w/ latest from ARCUtils

--- oregoninvasiveshotline/wsgi.py
+++ oregoninvasiveshotline/wsgi.py
@@ -2,26 +2,47 @@ import os
 import site
 import sys
 
-here = os.path.dirname(__file__)  # Directory containing this module
-root = os.path.dirname(here)      # One level up containing directory
 
-major, minor = sys.version_info[:2]
-site_packages = 'lib/python{major}.{minor}/site-packages'.format(**locals())
-site_packages = os.path.join(root, '.env', site_packages)
+def create_wsgi_application(root, settings_module=None, local_settings_file=None):
+    """Create a WSGI application anchored at ``root``.
 
-if not os.path.isdir(site_packages):
-    raise NotADirectoryError('Could not find virtualenv site-packages at {}'.format(site_packages))
+    ``root`` must contain a virtualenv at ``./.env``.
 
-# Add the virtualenv's site-packages to sys.path, ensuring its packages
-# take precedence over system packages (by moving them to the front of
-# sys.path after they're added).
-old_sys_path = list(sys.path)
-site.addsitedir(site_packages)
-new_sys_path = [item for item in sys.path if item not in old_sys_path]
-sys.path = new_sys_path + old_sys_path
+    ``settings_module`` is only used if the ``DJANGO_SETTINGS_MODULE``
+    environment variable is not already set.
 
-os.environ.setdefault('LOCAL_SETTINGS_FILE', os.path.join(root, 'local.cfg'))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oregoninvasiveshotline.settings')
+    Likewise, ``local_settings_file`` is only used if the environment
+    variable ``LOCAL_SETTINGS_FILE`` is not already set. The default
+    value for this is ``{root}/local.cfg``. If a project doesn't use
+    ``django-local-settings``, this will have no effect.
 
-from django.core.wsgi import get_wsgi_application  # noqa
-application = get_wsgi_application()
+    """
+    if local_settings_file is None:
+        local_settings_file = os.path.join(root, 'local.cfg')
+
+    major, minor = sys.version_info[:2]
+    site_packages = 'lib/python{major}.{minor}/site-packages'.format(**locals())
+    site_packages = os.path.join(root, '.env', site_packages)
+
+    if not os.path.isdir(site_packages):
+        raise NotADirectoryError(
+            'Could not find virtualenv site-packages at {}'.format(site_packages))
+
+    # Add the virtualenv's site-packages to sys.path, ensuring its packages
+    # take precedence over system packages (by moving them to the front of
+    # sys.path after they're added).
+    old_sys_path = list(sys.path)
+    site.addsitedir(site_packages)
+    new_sys_path = [item for item in sys.path if item not in old_sys_path]
+    sys.path = new_sys_path + old_sys_path
+
+    os.environ.setdefault('LOCAL_SETTINGS_FILE', local_settings_file)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+    from django.core.wsgi import get_wsgi_application
+    return get_wsgi_application()
+
+
+root = os.path.dirname(os.path.dirname(__file__))
+settings_module = 'oregoninvasiveshotline.settings'
+application = create_wsgi_application(root, settings_module)
