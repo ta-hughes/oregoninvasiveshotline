7cc1e0ddbf636533c3a4e2da83b0f08b5484460a
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-01-25 11:00:14 -0800

    Clean up species.models

--- oregoninvasiveshotline/species/models.py
+++ oregoninvasiveshotline/species/models.py
@@ -6,45 +6,57 @@ from django.db import models
 
 
 def category_id_to_species_id_json():
-    species = Species.objects.all().select_related("category").values_list("category__pk", "pk").order_by("category__pk")
+    species = (
+        Species.objects
+            .select_related('category')
+            .order_by('category__pk')
+            .values_list('category__pk', 'pk')
+    )
     category_id_to_species_id = defaultdict(list)
     for category_id, species_id in species:
         category_id_to_species_id[category_id].append(species_id)
-
     return json.dumps(category_id_to_species_id)
 
 
 class Category(models.Model):
-    """Simply a container for species"""
-    category_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    icon = models.ImageField(upload_to="icons", help_text="""
-        It is expected that you got the icon from http://mapicons.mapsmarker.com
-        and they have a transparent background, and a white foreground
-    """, null=True, blank=True)
+
+    """A container for species."""
 
     class Meta:
-        db_table = "category"
+        db_table = 'category'
         ordering = ['name']
+
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    icon = models.ImageField(
+        upload_to='icons',
+        null=True,
+        blank=True,
+        help_text=(
+            'It is expected that the icon came from http://mapicons.mapsmarker.com/ and that it '
+            'has a transparent background with a white foreground'
+        )
+    )
 
     def __str__(self):
         return self.name
 
 
 class Severity(models.Model):
-    """
-    This could be an enum, but in the original DB it was a table with 3 rows
-    for "native" "non-native" "invasive"
-    """
-    severity_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    color = models.CharField(max_length=7, help_text="An HTML color of the form '#rrggbb'", validators=[
-        RegexValidator(r"#[0-9A-Fa-f]{6}")
-    ])
 
     class Meta:
-        db_table = "severity"
+        db_table = 'severity'
         ordering = ['name']
+
+    severity_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    color = models.CharField(
+        max_length=7,
+        help_text='An HTML color of the form "#rrggbb"',
+        validators=[
+            RegexValidator(r"#[0-9A-Fa-f]{6}")
+        ]
+    )
 
     def __str__(self):
         return self.name
