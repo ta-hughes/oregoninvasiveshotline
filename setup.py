e0aea3dd280f6c7fd7ba07506ab62e2313c35a89
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-02-01 12:15:16 -0800

    Upgrade django-arcutils 2.0a11 => 2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'django-arcutils>=2.0a11',
+        'django-arcutils>=2.0.0',
         'django-bootstrap-form>=3.2',
         'django-cloak',
         'django-haystack>=2.4.1',
