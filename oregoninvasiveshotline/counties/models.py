7629ef31bdc6ce6cd8b8c00e50eccaa93f80ad6a
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-02-11 14:59:42 -0800

    Clean up the County model a little bit
    The Meta class should always be at the top in my opinion instead of in
    some random location.

--- oregoninvasiveshotline/counties/models.py
+++ oregoninvasiveshotline/counties/models.py
@@ -2,16 +2,17 @@ from django.contrib.gis.db import models
 
 
 class County(models.Model):
-    county_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    the_geom = models.MultiPolygonField(srid=4326)
-
-    objects = models.GeoManager()
 
     class Meta:
         db_table = 'county'
         ordering = ['state', 'name']
+
+    objects = models.GeoManager()
+
+    county_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    the_geom = models.MultiPolygonField(srid=4326)
 
     @property
     def label(self):
