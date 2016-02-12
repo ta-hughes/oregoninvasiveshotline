0d8a7abf961a6cd11cb854f5b0936ef46b822f48
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-02-11 17:53:30 -0800

    Clean up UserIndex

--- oregoninvasiveshotline/users/search_indexes.py
+++ oregoninvasiveshotline/users/search_indexes.py
@@ -4,25 +4,18 @@ from .models import User
 
 
 class UserIndex(indexes.SearchIndex, indexes.Indexable):
-    """
-    Indexes a user model defined by the fields below in Elasticsearch,
-    using Haystack
-    """
+
     text = indexes.CharField(document=True, use_template=True)
-    first_name = indexes.CharField(model_attr="first_name")
-    last_name = indexes.CharField(model_attr="last_name")
-    email = indexes.CharField(model_attr="email")
-    is_active = indexes.BooleanField(model_attr="is_active")
-    is_staff = indexes.BooleanField(model_attr="is_staff")
+    email = indexes.CharField(model_attr='email')
+    first_name = indexes.CharField(model_attr='first_name')
+    last_name = indexes.CharField(model_attr='last_name')
+
+    # Data fields (stored by not indexed)
+    is_active = indexes.BooleanField(model_attr='is_active', indexed=False)
+    is_staff = indexes.BooleanField(model_attr='is_staff', indexed=False)
 
     def get_model(self):
-        """
-        Returns the current model
-        """
         return User
 
     def index_queryset(self, using=None):
-        """
-        Used when the entire User index is updated, indexes new documents
-        """
         return self.get_model().objects.all()
