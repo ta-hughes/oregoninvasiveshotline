866aaf900429633d0fc6a80f22649274635f8565
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-02-12 13:18:56 -0800

    Clean up SpeciesIndex

--- oregoninvasiveshotline/species/search_indexes.py
+++ oregoninvasiveshotline/species/search_indexes.py
@@ -4,30 +4,19 @@ from .models import Species
 
 
 class SpeciesIndex(indexes.SearchIndex, indexes.Indexable):
-    """
-    Indexes species in Haystack -- Uses species "name" as
-    the primary search term
-    """
+
     text = indexes.CharField(document=True, use_template=True)
+    category = indexes.CharField(model_attr='category')
+    name = indexes.CharField(model_attr='name')
+    remedy = indexes.CharField(model_attr='remedy')
+    resources = indexes.CharField(model_attr='resources')
+    scientific_name = indexes.CharField(model_attr='scientific_name')
+    severity = indexes.CharField(model_attr='severity')
 
-    name = indexes.CharField(model_attr="name")
-    scientific_name = indexes.CharField(model_attr="scientific_name")
-    remedy = indexes.CharField(model_attr="remedy")
-    resources = indexes.CharField(model_attr="resources")
-
-    is_confidential = indexes.BooleanField(model_attr="is_confidential")
-
-    severity = indexes.CharField(model_attr="severity")
-    category = indexes.CharField(model_attr="category")
+    is_confidential = indexes.BooleanField(model_attr='is_confidential', indexed=False)
 
     def get_model(self):
-        """
-        Return the current model
-        """
         return Species
 
     def index_queryset(self, using=None):
-        """
-        Used when the entire Species index is updated, indexes new documents
-        """
         return self.get_model().objects.all()
