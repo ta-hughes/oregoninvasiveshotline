c2641cfa28d9a3745f7c19dec3eb2c8bc5cfecc7
Sean S <sisson@pdx.edu>
2016-02-11 23:13:39 -0800

    Make admin GIS map load over HTTPS
    By default, Django's `GeoModelAdmin` uses OpenLayers whose URL is not
    available via HTTPS. To get around this, we subclass GeoModelAdmin and
    swap the openlayers_url for one that's available over HTTPS.

--- oregoninvasiveshotline/reports/admin.py
+++ oregoninvasiveshotline/reports/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.gis.admin.options import GeoModelAdmin
 
 from .models import Report
 
-admin.site.register(Report)
+
+class CustomGeoModelAdmin(GeoModelAdmin):
+
+    # Use a non-default URL so OpenLayers can be used over HTTPS
+    openlayers_url = '//cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js'
+
+
+admin.site.register(Report, CustomGeoModelAdmin)
