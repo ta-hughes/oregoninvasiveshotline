f51081c1ed0a5a0e713e2ae4c3fdb6b07877d170
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-02-16 16:05:03 -0800

    Put user avatar colors in a separate module
    Instead of having an absurdly long line in users.views.

--- oregoninvasiveshotline/users/views.py
+++ oregoninvasiveshotline/users/views.py
@@ -14,6 +14,7 @@ from arcutils.db import will_be_deleted_with
 from oregoninvasiveshotline.reports.models import Invite, Report
 from oregoninvasiveshotline.utils import get_tab_counts
 
+from .colors import AVATAR_COLORS
 from .forms import PublicLoginForm, UserForm, UserSearchForm
 from .models import User
 from .perms import can_list_users, permissions
@@ -67,7 +68,7 @@ def authenticate(request):
     return redirect(request.GET.get("next") or settings.LOGIN_REDIRECT_URL)
 
 
-def avatar(request, user_id, colors="AliceBlue AntiqueWhite Aqua Aquamarine Azure Beige Bisque Black BlanchedAlmond Blue BlueViolet Brown BurlyWood CadetBlue Chartreuse Chocolate Coral CornflowerBlue Cornsilk Crimson Cyan DarkBlue DarkCyan DarkGoldenRod DarkGray DarkGreen DarkKhaki DarkMagenta DarkOliveGreen DarkOrange DarkOrchid DarkRed DarkSalmon DarkSeaGreen DarkSlateBlue DarkSlateGray DarkTurquoise DarkViolet DeepPink DeepSkyBlue DimGray DodgerBlue FireBrick FloralWhite ForestGreen Fuchsia Gainsboro GhostWhite Gold GoldenRod Gray Green GreenYellow HoneyDew HotPink IndianRed Indigo Ivory Khaki Lavender LavenderBlush LawnGreen LemonChiffon LightBlue LightCoral LightCyan LightGoldenRodYellow LightGray LightGreen LightPink LightSalmon LightSeaGreen LightSkyBlue LightSlateGray LightSteelBlue LightYellow Lime LimeGreen Linen Magenta Maroon MediumAquaMarine MediumBlue MediumOrchid MediumPurple MediumSeaGreen MediumSlateBlue MediumSpringGreen MediumTurquoise MediumVioletRed MidnightBlue MintCream MistyRose Moccasin NavajoWhite Navy OldLace Olive OliveDrab Orange OrangeRed Orchid PaleGoldenRod PaleGreen PaleTurquoise PaleVioletRed PapayaWhip PeachPuff Peru Pink Plum PowderBlue Purple RebeccaPurple Red RosyBrown RoyalBlue SaddleBrown Salmon SandyBrown SeaGreen SeaShell Sienna Silver SkyBlue SlateBlue SlateGray Snow SpringGreen SteelBlue Tan Teal Thistle Tomato Turquoise Violet Wheat White WhiteSmoke Yellow YellowGreen".split(" ")):  # noqa
+def avatar(request, user_id, colors=AVATAR_COLORS):
     """
     Generates an SVG to use as the user's default avatar, using some random
     colors based on the user's PK
