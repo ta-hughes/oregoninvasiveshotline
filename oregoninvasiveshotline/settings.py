15b4b2182339e8b58ee5eb764c10fc03d6ff8689
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-02-16 18:08:12 -0800

    Fix message tag for error messages

--- oregoninvasiveshotline/settings.py
+++ oregoninvasiveshotline/settings.py
@@ -1,8 +1,12 @@
 from django.conf import global_settings
+from django.contrib.messages import constants as messages
 
 from arcutils.settings import init_settings
 
 init_settings()
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 PASSWORD_HASHERS = list(global_settings.PASSWORD_HASHERS)
 PASSWORD_HASHERS.insert(1, 'oregoninvasiveshotline.utils.RubyPasswordHasher')
