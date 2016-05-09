ta-hughes/oregoninvasiveshotline

commit a6d2022632483a604c7341075ccb46b80da630fb

When auto-creating user, convert email address to lower case
This pertains to when a user is created automatically by submitting a
report or being invited to review a report.

Otherwise, it's too easy to inadvertantly create duplicate users.

NOTE: Technically, email addresses are case-sensitive, but I don't think
this is ever useful or relevant in practice.

XXX: There may be other places in the code where we need to make similar
changes.

oregoninvasiveshotline/reports/models.py
@@ -210,10 +210,12 @@ class Invite(models.Model):
         has already been invited before)
         """
         user_model = get_user_model()
-        try:
-            user = user_model.objects.get(email__iexact=email)
-        except user_model.DoesNotExist:
-            user = user_model.objects.create(email=email, is_active=False)
+
+        defaults = {
+            'email': email.lower(),
+            'is_active': False,
+        }
+        user = user_model.objects.get_or_create(email__iexact=email, defaults=defaults)
 
         if Invite.objects.filter(user=user, report=report).exists():
             return False
@@ -228,7 +230,7 @@ class Invite(models.Model):
                 "url": user.get_authentication_url(request, next=reverse("reports-detail", args=[report.pk]))
             }),
             "noreply@pdx.edu",
-            [email]
+            [user.email]
         )
 
         invite.save()

oregoninvasiveshotline/reports/forms.py
@@ -281,7 +281,7 @@ class ReportForm(forms.ModelForm):
         #       fix this).
         email = self.cleaned_data['email']
         defaults = {
-            'email': email,
+            'email': email.lower(),
             'prefix': self.cleaned_data.get('prefix', ''),
             'first_name': self.cleaned_data.get('first_name'),
             'last_name': self.cleaned_data.get('last_name'),

commit a2b6585359feb04b3c11ee6370bf27d599e573e0

Fix usage of get_or_create() in Invite.create()
get_or_create() returns (instance, created). Regression introduced in
a6d2022. Released in version 1.8.0 on 2/29/16.

oregoninvasiveshotline/reports/models.py
@@ -215,7 +215,7 @@ class Invite(models.Model):
             'email': email.lower(),
             'is_active': False,
         }
-        user = user_model.objects.get_or_create(email__iexact=email, defaults=defaults)
+        user, _ = user_model.objects.get_or_create(email__iexact=email, defaults=defaults)
 
         if Invite.objects.filter(user=user, report=report).exists():
             return False