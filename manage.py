2c200da678ab55a4308b5307705da09758013fa1
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-04-03 17:40:23 -0700

    Improve manage.py
    - Allow --env to be passed (makes setting LOCAL_SETTINGS_FILE easy)
    - Make local settings loader be quiet

--- manage.py
+++ manage.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
+import argparse
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oregoninvasiveshotline.settings")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=False, usage=argparse.SUPPRESS)
+    parser.add_argument('--env', default=None)
+    args, remaining_args = parser.parse_known_args()
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oregoninvasiveshotline.settings')
+    os.environ.setdefault('LOCAL_SETTINGS_CONFIG_QUIET', 'true')
+
+    if args.env:
+        local_settings_file = 'local.{args.env}.cfg'.format(args=args)
+        os.environ['LOCAL_SETTINGS_FILE'] = local_settings_file
 
     from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    execute_from_command_line([sys.argv[0]] + remaining_args)
