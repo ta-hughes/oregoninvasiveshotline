8647393ee3ce3bd8db0825217d59efaddb531970
Thom Linton <tlinton@pdx.edu>
2018-03-08 16:28:55 -0800

    Refactors fixture and media asset handling.

--- commands.py
+++ commands.py
@@ -15,11 +15,14 @@ from emcee.backends.aws.provision.services.remote import provision_database
 from emcee.backends.aws.deploy import AWSDjangoDeployer
 from emcee.backends.aws.infrastructure.commands import *
 
+DEFAULT_FIXTURES = 'counties.json'
+DEVELOPMENT_FIXTURES = 'dummy_user.json category.json severity.json pages.json'
+
 
 @command
 def loaddata(config):
-    manage(config,
-           'loaddata dummy_user.json category.json severity.json counties.json pages.json')
+    manage(config, 'loaddata {}'.format(DEFAULT_FIXTURES))
+    manage(config, 'loaddata {}'.format(DEVELOPMENT_FIXTURES))
 
 
 @command(env='dev', timed=True)
@@ -29,8 +32,8 @@ def init(config, overwrite=False):
     # provision_database_local(config, drop=overwrite, with_postgis=True)
     manage(config, 'migrate --no-input')
     loaddata(config)
-    manage(config, 'rebuild_index --noinput')
     manage(config, 'generate_icons --no-input --clean --force')
+    manage(config, 'rebuild_index --noinput')
     # test(config, with_coverage=True, force_env='test')
 
 
@@ -38,24 +41,28 @@ class Deployer(AWSDjangoDeployer):
     def bootstrap_application(self):
         super(Deployer, self).bootstrap_application()
 
-        # Create media directory on EBS mount, link to it from app root
-        # and apply the correct permissions.
-        
+        # Install static/managed record data
+        manage_remote(self.config, 'loaddata {}'.format(DEFAULT_FIXTURES))
 
+        # Create media directory on EBS mount and link to app's media root
         remote(self.config, ('mkdir', '-p', '/vol/store/media'), sudo=True)
         remote(self.config, ('mkdir', '-p', '{remote.path.root}/media'), sudo=True)
-        remote(self.config, ('ln', '-sf', '/vol/store/media', '{remote.path.media}'), sudo=True)
-        remote(self.config, ('chown', '-R', '{service.user}:nginx', '/vol/store/media'), sudo=True)
+        remote(self.config, ('test', '-h', '{remote.path.media}', '||',
+                             'ln', '-sf', '/vol/store/media', '{remote.path.media}'), sudo=True)
 
         # Synchronize icons and assorted media assets
-        rsync(self.config, 'media/*', self.config.remote.path.media)
-
-        # Rebuild search index
-        manage_remote(self.config, 'rebuild_index --noinput')
+        rsync(self.config, 'media/icons', self.config.remote.path.media)
+        rsync(self.config, 'media/generated_icons', self.config.remote.path.media)
+        rsync(self.config, 'media/generated_thumbnails', self.config.remote.path.media)
 
         # Generate icons
         manage_remote(self.config, 'generate_icons --no-input')
 
+        # Set the correct permissions on generated assets
+        remote(self.config, ('chown', '-R', '{service.user}:nginx', '/vol/store/media'), sudo=True)
+
+        # Rebuild search index
+        manage_remote(self.config, 'rebuild_index --noinput')
 
 @command(env=True)
 def deploy_app(config, provision=False, createdb=False):
