f7ecb8200b5c63737454ff7cf642681d59423e12
Thom Linton <tlinton@pdx.edu>
2018-06-20 17:09:37 -0700

    Adds celery worker to docker configuration and environment.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,13 +3,10 @@ set -e
 
 
 cd ${PROJECT_DIR}
-python3 -m venv ${WSGI_VENV}
+flock ${APP_VENV}.lock -c "${APP_VENV}/bin/mc -e docker init"
 
-${WSGI_VENV}/bin/pip install wheel
-${WSGI_VENV}/bin/pip install -r requirements.txt
-${WSGI_VENV}/bin/mc -e docker manage "migrate --no-input"
-${WSGI_VENV}/bin/mc -e docker loaddata
-${WSGI_VENV}/bin/mc -e docker manage "rebuild_index --noinput"
-${WSGI_VENV}/bin/mc -e docker manage "generate_icons --no-input --clean --force"
-
-exec ${WSGI_VENV}/bin/python manage.py runserver 0.0.0.0:8000
+if [[ ${APP_SERVICE} == "wsgi" ]]; then
+    exec ${APP_VENV}/bin/python manage.py runserver 0.0.0.0:8000
+elif [[ ${APP_SERVICE} == "celery" ]]; then
+    exec ${APP_VENV}/bin/celery -A oregoninvasiveshotline worker -l INFO
+fi
