1f4eb6be6df3f0edb4544cbaba57b62b40805568
Brandon Conway <conwayb@pdx.edu>
2018-10-19 13:03:10 -0700

    Manage Google API keys using AWS ssm and emcee
    In development environments (native or docker) you should export
    an environment variable, eg:
    
        export GOOGLE_API_KEY='{ key }'
    
    A dev key can be found in the google developer console or
    you can create and supply your own.
    
    API keys for stage and production environments are stored and
    fetched from AWS key store; you will be prompted to supply these
    keys when you provision infrastructure for a given environment.

--- oregoninvasiveshotline/settings.py
+++ oregoninvasiveshotline/settings.py
@@ -1,14 +1,34 @@
+import os
+
 from django.conf import global_settings
 
 from arcutils.settings import init_settings
 from emcee.backends.aws import processors
 
 
+def set_google_map_url(settings):
+    env = settings["ENV"]
+    if env == 'prod' or env == 'stage':
+        GOOGLE_API_KEY = processors.ssm(
+            'GOOGLE_API_KEY', ssm_prefix=settings['SSM_KEY'], region=settings['AWS_REGION'])
+        settings["GOOGLE"]["maps"] = {
+            "url": "//maps.googleapis.com/maps/api/js?key={}".format(GOOGLE_API_KEY)}
+    else:
+        try:
+            GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+            settings["GOOGLE"]["maps"] = {
+                "url": "//maps.googleapis.com/maps/api/js?key={}".format(GOOGLE_API_KEY)}
+        except KeyError:
+            raise KeyError(
+                'You need to export a GOOGLE_API_KEY environment variable in development environments')
+
+
 init_settings(settings_processors=[processors.set_secret_key,
                                    processors.set_sentry_dsn,
                                    processors.set_smtp_parameters,
                                    processors.set_database_parameters,
-                                   processors.set_elasticsearch_parameters])
+                                   processors.set_elasticsearch_parameters,
+                                   set_google_map_url])
 
 PASSWORD_HASHERS = list(global_settings.PASSWORD_HASHERS)
 PASSWORD_HASHERS.insert(1, 'oregoninvasiveshotline.hashers.RubyPasswordHasher')
