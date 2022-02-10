917b2ac93aa7222f99dc1b713c6082b5c1558156
Thom Linton <tlinton@pdx.edu>
2022-02-10 15:58:48 -0800

    Prepares release 1.15.5.

--- oregoninvasiveshotline/__init__.py
+++ oregoninvasiveshotline/__init__.py
@@ -5,6 +5,6 @@ if sys.version_info < (3, 7):
     raise Exception('ORO requires Python version 3.7 or later.')
 
 
-__version__ = '1.15.4'
+__version__ = '1.15.5'
 __author__ = 'OIT Web & Mobile Team, Portland State University'
 __homepage__ = 'https://psu-arc-oit.github.io/oregoninvasiveshotline'
