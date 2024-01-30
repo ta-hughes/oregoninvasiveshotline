a5ca27492c5c7c3f180189d27899c0364d29e2a5
Thom Linton <tlinton@pdx.edu>
2024-01-30 15:50:59 -0800

    Adds support for PEP 517, PEP 518, PEP 621, PEP 660.

--- oregoninvasiveshotline/__init__.py
+++ oregoninvasiveshotline/__init__.py
@@ -1,10 +1,5 @@
 """Oregon Invasives Hotline"""
 import sys
 
-if sys.version_info < (3, 7):
-    raise Exception('ORO requires Python version 3.7 or later.')
 
-
-__version__ = '1.15.14'
-__author__ = 'OIT Web & Mobile Team, Portland State University'
-__homepage__ = 'https://psu-arc-oit.github.io/oregoninvasiveshotline'
+__version__ = '1.16.0'
